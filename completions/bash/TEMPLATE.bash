7228c43aeb504c8be3cb68238490775d9a2c9bb6
acly7946 <66972514+acly7946@users.noreply.github.com>
2022-05-17 09:30:35 +0000

    Tab indentation

--- completions/bash/TEMPLATE.bash
+++ completions/bash/TEMPLATE.bash
@@ -2,36 +2,36 @@
 
 _TEMPLATE()
 {
-  local cur prev short long
-  _get_comp_words_by_ref cur prev
+	local cur prev short long
+	_get_comp_words_by_ref cur prev
 
-  short=(
-    -h
-    -V
-  )
+	short=(
+		-h
+		-V
+	)
 
-  long=(
-    --help
-    --version
-  )
+	long=(
+		--help
+		--version
+	)
 
-  case $prev in
-    -c|--config)
-      _filedir
-      return
-      ;;
-  esac
+	case $prev in
+		-c|--config)
+			_filedir
+			return
+			;;
+	esac
 
-  if [[ $cur == --* ]]; then
-    COMPREPLY=($(compgen -W "${long[*]}" -- "$cur"))
-  elif [[ $cur == -* ]]; then
-    COMPREPLY=($(compgen -W "${short[*]}" -- "$cur"))
-    COMPREPLY+=($(compgen -W "${long[*]}" -- "$cur"))
-  else
-    COMPREPLY=($(compgen -W "${short[*]}" -- "$cur"))
-    COMPREPLY+=($(compgen -W "${long[*]}" -- "$cur"))
-    COMPREPLY+=($(compgen -c -- "$cur"))
-  fi
+	if [[ $cur == --* ]]; then
+		COMPREPLY=($(compgen -W "${long[*]}" -- "$cur"))
+	elif [[ $cur == -* ]]; then
+		COMPREPLY=($(compgen -W "${short[*]}" -- "$cur"))
+		COMPREPLY+=($(compgen -W "${long[*]}" -- "$cur"))
+	else
+		COMPREPLY=($(compgen -W "${short[*]}" -- "$cur"))
+		COMPREPLY+=($(compgen -W "${long[*]}" -- "$cur"))
+		COMPREPLY+=($(compgen -c -- "$cur"))
+	fi
 
 } &&
 complete -F _TEMPLATE TEMPLATE
